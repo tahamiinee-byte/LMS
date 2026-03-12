5b80505181dbadda746d10e24559233af3189ad2
OurabiaMehdi <yonkodme@example.com>
2026-03-12 16:00:23 +0100

    tajroba

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const path = require('path')
 const app = require(path.resolve(__dirname, 'src/app.js'))
-const {config} = require(path.resolve(__dirname,'./src/config/config.js'))
 
-
-app.listen(config.port , ()=>{
-    console.log(`server runnning on ${config.port}`)
+app.listen(5000, ()=>{
+    console.log(`server runnning on ${5000}`)
 })
