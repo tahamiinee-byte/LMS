2ac76e2157aaf7fd4198c590a64964a003b60ec9
Taha <tahamiinee@gmail.com>
2026-03-12 15:07:26 +0100

    fixing the config.js

--- src/config/config.js
+++ src/config/config.js
@@ -10,7 +10,7 @@ const config = {
 const dbconfig = {
     host : process.env.DB_HOST,
     port :process.env.DB_PORT,
-    user : process.env.DB_ ,
+    user : process.env.DB_USER ,
     password : process.env.DB_PASSWORD , 
     database : process.env.DB_NAME 
 }
