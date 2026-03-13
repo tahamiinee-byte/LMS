5e9b9345f96b67fe7358e06f0f132649d329ff64
Taha <tahamiinee@gmail.com>
2026-03-13 12:17:12 +0100

    Adding {db , routes , controllers} folders in src folder

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const path = require("path")
 const app = express()
-
-
-
 app.use(express.static(path.resolve(__dirname,'../public')))
+
+
+app.use(express.json())
 app.use(express.urlencoded({extended : false }))
 
+app.post('/login' , (req , res)=>{
+    console.log(req.body)
+    
+})
 module.exports = app 
 
