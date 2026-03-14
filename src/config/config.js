c7e9a6957bce02e5be88c627ceed502d93380968
Taha <tahamiinee@gmail.com>
2026-03-14 14:53:57 +0100

    This is the right code

--- src/config/config.js
+++ src/config/config.js
@@ -3,12 +3,10 @@ const path = require('path')
 
 dotenv.config({path: path.resolve(__dirname,'../../.env')});
 
-<<<<<<< HEAD
-=======
+
 const config = {
     port : process.env.PORT || 5000 , 
 }
->>>>>>> f10ff3b (fixin a small issue)
 const dbconfig = {
     host : process.env.DB_HOST,
     port :process.env.DB_PORT,
@@ -17,11 +15,8 @@ const dbconfig = {
     database : process.env.DB_NAME 
 }
 
-<<<<<<< HEAD
-module.exports = {dbconfig};
-=======
+
 const sessionConfig = {
     secret : process.env.SECRET
 }
 module.exports = {config ,dbconfig, sessionConfig};
->>>>>>> 283d5ba (Creating a session)
