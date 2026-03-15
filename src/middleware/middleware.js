3fd4d0c6a5cf4227f93eb3d15797060b51355d81
Taha <tahamiinee@gmail.com>
2026-03-15 12:15:56 +0100

    adding middlware folder and a authentification middleware

---
+++ src/middleware/middleware.js
@@ -0,0 +1,11 @@
+const query = require("../db/connection.js");
+async function requireAuth (req , res , next) {
+    const id = req.session.UserID; 
+    if (!id) return res.redirect('/')
+    const ChechUserID = await query("SELECT EXISTS (SELECT 1 FROM person where id = $1 );" , [id]) 
+    if (!ChechUserID.rows[0].exists) {
+        return res.redirect("/")
+    }
+    next();
+}
+module.exports =requireAuth 
