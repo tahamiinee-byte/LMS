78345158e3d43818f25a085807f59b6624fadf60
Taha <tahamiinee@gmail.com>
2026-03-18 11:33:54 +0100

    Adding the module controller file that handles modules functionality

---
+++ src/controllers/module.js
@@ -0,0 +1,30 @@
+const {exec}= require('child_process')
+const path  =require('path')
+
+const getModuleData = (req,res) => {
+    // I Need to get data form the path public/{module_name}/{module_type_data}
+    const name = req.params.module_name
+    const type = req.params.type
+    const dirPath= path.join(__dirname, '../../modules', name, type);
+    exec(`ls -t ${dirPath}`, (error,stdout,stderr) =>{
+        if(error) {
+            console.error('EXEC ERROR', error.message)
+        }
+        else if (stderr){
+            console.error("STDERR:", stderr);
+        }
+        
+        const filesName = stdout.split('\n').filter(Boolean)
+        console.log(filesName)
+        res.json(filesName);
+    })
+}
+const downloadData = (req,res)=>{
+    console.log('Here I am ')
+    const {module_name, type , fileName} = req.params.module_name
+    const filepath = path.join(__dirname ,`../../modules/${module_name}/${type}/${fileName}`)
+    res.download(filepath ,(err) =>{
+        if (err) res.status(404).send(`${fileName} Not found`)
+    })
+}
+module.exports = {getModuleData,downloadData}
